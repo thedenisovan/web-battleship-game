d3ecc067e6097bfdf7f6e10e165090e8ec3908dd
Dainis Dilevka <thedenisovan@proton.me>
2025-07-25 16:31:53 +0100

    Added diplay render of ships on field

--- src/controls.js
+++ src/controls.js
@@ -1,2 +1,18 @@
-import {Ship, GameBoard} from './gamePlay.js'
+import {Ship, GameBoard, Player} from './gamePlay.js'
+
+export let player1 = new Player();
+export let computer = new Player();
+
+player1.gameBoard.placeShip(4, [0, 0], false);
+player1.gameBoard.placeShip(3, [2, 6], true);
+player1.gameBoard.placeShip(3, [8, 6], false);
+player1.gameBoard.placeShip(2, [7, 1], false);
+player1.gameBoard.placeShip(5, [4, 4], true);
+
+computer.gameBoard.placeShip(4, [1, 8], true);
+computer.gameBoard.placeShip(3, [6, 1], false);
+computer.gameBoard.placeShip(2, [3, 3], true);
+computer.gameBoard.placeShip(5, [0, 2], false);
+computer.gameBoard.placeShip(3, [6, 6], false);
+
 
--- src/ui.js
+++ src/ui.js
@@ -1,22 +1,41 @@
+import { player1, computer } from './controls.js';
+import { Ship } from './gamePlay.js'
+
+const backgroundImg = require('./assets/background.jpg');
+
 // For each battlefield create a grid of 10x10 cells
-const field = document
-  .querySelectorAll('[data-battlefield]')
-  .forEach((f) => createGrid(10, f));
+document.querySelectorAll('[data-battlefield]').forEach((field) => {
+    createGrid(10, field);
+    field.style.background = backgroundImg;
+  });
 
 function createGrid(size, element) {
   for (let i = 0; i < size * size; i++) {
-    let cell = document.createElement('div');
+    const cell = document.createElement('div');
 
     cell.style.height = `${100 / size}%`;
     cell.style.width = `${100 / size}%`;
     cell.classList.add('cell');
-    cell.id = i < 10 ? '0' + i : i;
+    cell.id = i < 10 ? '0' + i : i; // Id to use as idx of battleships
     element.appendChild(cell);
   }
 }
 
-let cel = document.querySelectorAll('.cell').forEach((c) => {
-  c.addEventListener('click', () => {
-    console.log(c.id[0]);
-  });
+document.querySelectorAll('.battlefield-right .cell').forEach((cell) => {
+  if (computer.gameBoard.board[cell.id[0]][cell.id[1]] instanceof Ship) {
+      cell.style.background = 'rgba(250, 4, 28, 0.49)';
+    }
+});
+document.querySelectorAll('.battlefield-left .cell').forEach((cell) => {
+  if (player1.gameBoard.board[cell.id[0]][cell.id[1]] instanceof Ship) {
+      cell.style.background = 'rgba(250, 4, 28, 0.49)';
+    }
+});
+// Creates squared blocks representing a ship
+document.querySelectorAll('[data-ship]').forEach((ship) => {
+  for (let i = 0; i < ship.id; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    ship.appendChild(square);
+  }
 });
